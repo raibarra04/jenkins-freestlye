15859fd1e4a09d97e9104a177f2bacbecdf27f35
Robinson Ibarra <ribarra764@gmail.com>
2026-03-08 15:08:46 -0700

    feat: no-ref: added env variable for dynamic image name

--- script.groovy
+++ script.groovy
@@ -28,7 +28,7 @@ def buildImage() {
 
 def deployApp() {
     echo "Deploying the application to EC2..."
-    def shellCmd = "bash ./server-cmds.sh"
+    def shellCmd = "bash ./server-cmds.sh ${IMAGE_NAME}"
     sshagent(['ec2-server-key']) {
         sh "scp server-cmds.sh ec2-user@3.17.190.15:/home/ec2-user"
         sh "scp docker-compose.yaml ec2-user@3.17.190.15:/home/ec2-user"
