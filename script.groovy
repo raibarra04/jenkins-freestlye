0678830cc4c88a59f24a37df340d5a5f57e13090
Robinson Ibarra <ribarra764@gmail.com>
2026-03-08 14:48:36 -0700

    feat: no-ref: added shell script

--- script.groovy
+++ script.groovy
@@ -28,10 +28,11 @@ def buildImage() {
 
 def deployApp() {
     echo "Deploying the application to EC2..."
-    def dockerComposeCmd = "docker-compose -f docker-compose.yaml up --detach"
+    def shellCmd = "bash ./server-cmds.sh"
     sshagent(['ec2-server-key']) {
+        sh "scp server-cmds.sh ec2-user@3.17.190.15:/home/ec2-user"
         sh "scp docker-compose.yaml ec2-user@3.17.190.15:/home/ec2-user"
-        sh "ssh -o StrictHostKeyChecking=no ec2-user@3.17.190.15 ${dockerComposeCmd}"
+        sh "ssh -o StrictHostKeyChecking=no ec2-user@3.17.190.15 ${shellCmd}"
     }
 }
 
